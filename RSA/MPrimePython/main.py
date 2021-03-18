fe092054368c173f7b10e7e5f4da1e2626416302
RhuanFerrer <rhuanferrerr@gmail.com>
2021-03-18 16:56:59 -0300

    MPrime in python

---
+++ RSA/MPrimePython/main.py
@@ -0,0 +1,58 @@
+import math
+
+def inverse(num,mod):
+    out = 0
+    while True:
+        out = out + 1
+        if (num * out) % mod == 1:
+            return out
+
+def coprimo(num):
+    for out2 in range(2,num):
+        if math.gcd(num,out2) == 1:
+            return out2
+
+quant = int(input("Quantos numeros primos?"))
+
+p = []
+for i in range(quant):
+    p.append(int(input()))
+print(p)
+
+N = 1
+for i in range(len(p)):
+    N *= p[i]
+
+print("N = ", N)
+
+fiN = 1
+for i in range(len(p)):
+    fiN *= p[i] - 1
+
+print("fiN = ", fiN)
+ 
+e = coprimo(fiN)
+print("e = ", e)
+
+d = inverse(e,fiN)
+print("d = ", d)
+
+C = pow(int(input("insira a mensagem:")),e)%N
+print("C = ", C)
+
+dn = []
+for i in range(len(p)):
+    dn.append(d % (p[i] - 1))
+print("dn = ",dn)
+
+Mn = []
+for i in range(len(p)):
+    Mn.append((pow(C,dn[i]))%p[i])
+print("Mn = ",Mn)
+
+x = 0
+for i in range(len(p)):
+    x += Mn[i] * N / p[i] * inverse(N / p[i], p[i])
+x = x % N
+
+print(x)
